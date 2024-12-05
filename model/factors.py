18df0ed1e93a43071d4a530714e24c1521362743
vdella <vitor.origamer@gmail.com>
2024-12-05 17:08:46 -0300

    added control factors file

---
+++ model/factors.py
@@ -0,0 +1,6 @@
+control_factors = {
+    "operating_temperature": [('1', 313), ('2', 323), ('3', 333), ('4', 338), ('5', 343), ('6', 353)],  # 6 levels
+    "fuel_flow_rate": [('1', 50), ('2', 65), ('3', 85), ('4', None), ('5', None), ('6', None)],  # 3 levels
+    "air_flow_rate": [('1', 300), ('2', 400), ('3', 500), ('4', None), ('5', None), ('6', None)],  # 3 levels
+    "fuel_supply_pressure": [('1', 1.5), ('2', 2.0), ('3', 2.5), ('4', None), ('5', None), ('6', None)],  # 3 levels
+}
